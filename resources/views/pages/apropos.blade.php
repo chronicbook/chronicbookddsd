31694346f612d7e3690a04d19e7abc3fb56c3cc2
chronicbook <cabrelwilly20@gmail.com>
2022-06-11 04:44:53 +0100

    ajout des images

--- resources/views/pages/apropos.blade.php
+++ resources/views/pages/apropos.blade.php
@@ -6,5 +6,6 @@
 @section ('content')
 
     <h1>bonjour ici ce sont les contact </h1>
+    <img src="/images/image.jpg" alt="image inconue">
     <p><a href="{{ route('app_home') }}">revenir a la page d'acceuiel</a></p>
 @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,7 +2,9 @@
 
 
 @section ('content')
+
             <h1>bonjour tous chronicbook</h1>
+            <img src="/images/image2.jpg" alt="image inconue">
             <p> la date d'aujourdhuit est {{ date ('d/m/y') }}</p>
 
 @endsection
